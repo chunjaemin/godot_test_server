4ebdda53043f683d6c47e09729bcc7d6918aaf1a
chunjaemin <chun4582@naver.com>
2024-10-22 01:49:14 +0900

    network_finish

--- express.js
+++ express.js
@@ -31,20 +31,20 @@ wss.on('connection', (ws) => {
   ws.on('message', (data) => {
     try {
       // 자이로스코프 데이터는 JSON 문자열로 전송되므로, 이를 파싱
-      const parsedData = JSON.parse(data);
-
-      if (parsedData.event === 'type_phone') {
+      let parsedData = JSON.parse(data);
+      if (parsedData.event == 'type_phone') {
         client_phone = user;
+        alertconnect();
       }
 
-      if (parsedData.event === 'type_computer') {
+      if (parsedData.event == 'type_computer') {
         client_computer = user;
+        alertconnect();
       }
       
       if (parsedData.event === 'gyroscope_data') {
-        console.log(`Received gyroscope data: x=${parsedData.x}, y=${parsedData.y}, z=${parsedData.z}`);
         if (client_computer) {
-          client_computer.send(data);
+          client_computer.send(JSON.stringify(parsedData));
         }
       } 
       
@@ -56,5 +56,19 @@ wss.on('connection', (ws) => {
   // 클라이언트 연결 종료 이벤트 처리
   ws.on('close', () => {
     console.log('Client disconnected');
+    if (ws == client_computer) {
+      client_computer = null;
+    } else if (ws == client_phone) {
+      client_phone = null;
+    }
   });
 });
+
+function alertconnect () {
+  if (client_phone && client_computer) {
+    let data = {
+      event : "alertconnect",
+    }
+    client_computer.send(JSON.stringify(data));
+  }
+}
